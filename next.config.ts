d6c1c3c5bb53d83610ba8ae03abd13d42e22f6e1
ritessshhh <chavanritesh20@gmail.com>
2025-11-26 07:08:27 -0500

    Remove basePath for username.github.io deployment

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/RoyyRitesh',
-  assetPrefix: '/RoyyRitesh',
   images: {
     unoptimized: true,
     remotePatterns: [
